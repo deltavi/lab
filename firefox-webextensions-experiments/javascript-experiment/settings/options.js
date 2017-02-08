19f3d7cc9f839351ddd80b07cda0872db32c2bf8
Vincenzo De Vivo <deltavi79@gmail.com>
2017-02-08 18:38:11 +0100

    option editing

--- firefox-webextensions-experiments/javascript-experiment/settings/options.js
+++ firefox-webextensions-experiments/javascript-experiment/settings/options.js
@@ -45,7 +45,7 @@ function storeScript(scriptName, scriptContent) {
 }
 
 
-function displayScript(scriptName, scriptContent) {
+/*function displayScript(scriptName, scriptContent) {
     var itemContainer = document.createElement('div');
     var itemName = document.createElement('h2');
     var itemScript = document.createElement('p');
@@ -56,6 +56,120 @@ function displayScript(scriptName, scriptContent) {
     itemContainer.appendChild(itemName);
     itemContainer.appendChild(itemScript);
     scriptsContainer.appendChild(itemContainer);
+}*/
+function displayScript(scriptName, scriptContent) {
+
+  /* create note display box */
+  var note = document.createElement('div');
+  var noteDisplay = document.createElement('div');
+  var noteH = document.createElement('h2');
+  var notePara = document.createElement('p');
+  var deleteBtn = document.createElement('button');
+  var clearFix = document.createElement('div');
+
+  note.setAttribute('class','note');
+
+  noteH.textContent = scriptName;
+  notePara.textContent = scriptContent;
+  deleteBtn.setAttribute('class','delete');
+  deleteBtn.textContent = 'Delete note';
+  clearFix.setAttribute('class','clearfix');
+
+  noteDisplay.appendChild(noteH);
+  noteDisplay.appendChild(notePara);
+  noteDisplay.appendChild(deleteBtn);
+  noteDisplay.appendChild(clearFix);
+
+  note.appendChild(noteDisplay);
+
+  /* set up listener for the delete functionality */
+
+  deleteBtn.addEventListener('click',function(e){
+    evtTgt = e.target;
+    evtTgt.parentNode.parentNode.parentNode.removeChild(evtTgt.parentNode.parentNode);
+    browser.storage.local.remove(scriptName);
+  })
+
+  /* create note edit box */
+  var noteEdit = document.createElement('div');
+  var noteTitleEdit = document.createElement('input');
+  var noteBodyEdit = document.createElement('textarea');
+  var clearFix2 = document.createElement('div');
+
+  var updateBtn = document.createElement('button');
+  var cancelBtn = document.createElement('button');
+
+  updateBtn.setAttribute('class','update');
+  updateBtn.textContent = 'Update note';
+  cancelBtn.setAttribute('class','cancel');
+  cancelBtn.textContent = 'Cancel update';
+
+  noteEdit.appendChild(noteTitleEdit);
+  noteTitleEdit.value = scriptName;
+  noteEdit.appendChild(noteBodyEdit);
+  noteBodyEdit.textContent = scriptContent;
+  noteEdit.appendChild(updateBtn);
+  noteEdit.appendChild(cancelBtn);
+
+  noteEdit.appendChild(clearFix2);
+  clearFix2.setAttribute('class','clearfix');
+
+  note.appendChild(noteEdit);
+
+  scriptsContainer.appendChild(note);
+  noteEdit.style.display = 'none';
+
+  /* set up listeners for the update functionality */
+
+  noteH.addEventListener('click',function(){
+    noteDisplay.style.display = 'none';
+    noteEdit.style.display = 'block';
+  })
+
+  notePara.addEventListener('click',function(){
+    noteDisplay.style.display = 'none';
+    noteEdit.style.display = 'block';
+  })
+
+  cancelBtn.addEventListener('click',function(){
+    noteDisplay.style.display = 'block';
+    noteEdit.style.display = 'none';
+    noteTitleEdit.value = scriptName;
+    noteBodyEdit.value = scriptContent;
+  })
+
+  updateBtn.addEventListener('click',function(){
+    if(noteTitleEdit.value !== scriptName || noteBodyEdit.value !== scriptContent) {
+      updateNote(scriptName,noteTitleEdit.value,noteBodyEdit.value);
+      note.parentNode.removeChild(note);
+    }
+  });
+}
+
+
+/* function to update notes */
+
+function updateNote(delNote,newTitle,newBody) {
+  var storingNote = browser.storage.local.set({ [newTitle] : newBody });
+  storingNote.then(() => {
+    if(delNote !== newTitle) {
+      var removingNote = browser.storage.local.remove(delNote);
+      removingNote.then(() => {
+        displayNote(newTitle, newBody);
+      }, onError);
+    } else {
+      displayNote(newTitle, newBody);
+    }
+  }, onError);
+}
+
+/* Clear all notes from the display/storage */
+
+function clearAll() {
+  while (scriptsContainer.firstChild) {
+      scriptsContainer.removeChild(scriptsContainer.firstChild);
+  }
+  browser.storage.local.clear();
 }
 
 
